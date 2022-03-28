97d69459215cd7abd4d0f348597ee0ee3c301058
Victor Inojosa <vijoin@gmail.com>
2022-03-28 11:05:09 -0300

    Create a simple storage to test Struct and Mappings

---
+++ test/SimpleStorageTest.js
@@ -0,0 +1,7 @@
+// Store a number into favoriteNumber and retrieve it
+// Add a Person and validate that it's stored in people and nameToFavoriteNumber
+// Add several Persons and validate that they're stored in people and nameToFavoriteNumber
+// Test fail: people out of Index
+// Test fail: key not in mapping
+
+
